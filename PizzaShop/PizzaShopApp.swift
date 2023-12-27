b6f0fbf6712deecf15fa4b83f205000edae29ddb
Vitalya <vitalyatihonov@mail.ru>
2023-12-27 19:19:03 +0900

    Initial Commit

---
+++ PizzaShop/PizzaShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaShopApp.swift
+//  PizzaShop
+//
+//  Created by пользователь on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PizzaShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
